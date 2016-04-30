7ad5a077d2ad43c0989d8fbc96fc3d8ba1921281
Carl-Philip Majgaard <cmajgaar@colby.edu>
2016-04-30 00:07:02 +0000

    adds fields to user field and makes migrations

--- fifo/users/models.py
+++ fifo/users/models.py
@@ -7,13 +7,18 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+import uuid
+
 
 @python_2_unicode_compatible
 class User(AbstractUser):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    org_id = models.IntegerField(_('Organization ID'), blank=False)
+    name = models.CharField(_('Name of User'), blank=False, max_length=255)
+    
+    phone = models.CharField(_('Phone Number'), max_length=10, blank=True)
 
     def __str__(self):
         return self.username
